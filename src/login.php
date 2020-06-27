f624f3cf0dcb7690a102cf0a64fe309bf5131c0b
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2020-06-27 16:50:24 +0200

    added php files as iframes

--- src/login.php
+++ src/login.php
@@ -1,8 +1,3 @@
 <?php
 
-error_reporting(E_ALL);
-header("Strict-Transport-Security: max-age=16070400; includeSubDomains");
-
-session_start();
-
-return json_encode("true");
+echo json_encode($_REQUEST);
